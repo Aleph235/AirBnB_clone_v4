70faaa894a17b701afeb93a26e478123c594a9ac
Jebeniani <aymenjebeniani05@gmail.com>
2022-09-25 23:07:04 +0100

    js script

---
+++ web_dynamic/static/scripts/3-hbnb.js
@@ -0,0 +1,28 @@
+$(document).ready(function () {
+  let amenityIds = {};
+
+  $('input[type=checkbox]').click(function () {
+    if ($(this).prop('checked') === true) {
+      amenityIds[$(this).data('id')] = $(this).data('name');
+    } else if (!$(this).prop('checked') === false) {
+      delete amenityIds[$(this).attr('data-id')];
+    }
+    $('div.amenities h4').text(Object.values(amenityIds).join(', ')); 
+  });
+$.ajax({
+    url: 'http://0.0.0.0:5001/api/v1/status/',
+    success: function (data, textStatus) {
+      console.log(data.status);
+      if (data.status ==='success') {
+        $('#api-status').addClass('available');
+      } else {
+      $('#api-status').removeClass('available');
+      }
+    }
+  });
+$.ajax({
+  url: 'http://0.0.0.0:5001/api/v1/places_search',
+  type: 'POST',
+  contentType: 'application/json'
+    });
+});
